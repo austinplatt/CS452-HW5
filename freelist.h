36550ae224c0663410ee68d2673cff2f37027fd1
austinplatt <austinplatt@u.boisestate.edu>
2023-04-27 21:59:39 -0600

    first commit

--- freelist.h
+++ freelist.h
@@ -3,12 +3,17 @@
 
 typedef void *FreeList;
 
-extern FreeList freelistnew(unsigned int size, int l, int u);
+extern FreeList freelistnew(unsigned int size, void *base, int l, int u);
 
-extern void *freelistalloc(FreeList f, void *base, int e, int l);
+extern void *freelistalloc(FreeList f, void *base, int e, int l, int u);
 extern void freelistfree(FreeList f, void *base, void *mem, int e, int l);
 extern int freelistsize(FreeList f, void *base, void *mem, int l, int u);
 
 extern void freelistprint(FreeList f, unsigned int size, int l, int u);
+extern void printNode(void* node);
+static void updateHead(FreeList f, void* mem, int index);
+static void split(FreeList f, void *base, void* mem, int i, int deltaE, int u, int l);
+static void splitToLevel(FreeList f, void *base, void* mem, int sLevel, int currLevel,int offset);
+
 
 #endif
